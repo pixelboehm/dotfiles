a4aface70a98e7e2b10e98f4c3ca3b980d9e702c
Maximilian Fischer <maximilian.fischer@uni-due.de>
2022-11-14 10:20:18 +0100

    use quotes in grep

--- scripts/kontoauszuege.sh
+++ scripts/kontoauszuege.sh
@@ -4,12 +4,12 @@ YEAR=2021
 
 for f in *\ *.pdf; do mv "$f" "${f// /_}"; done
 
-for i in $(find ./ -print | grep -e 7559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf)
+for i in $(find ./ -print | grep -e "7559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
     mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
 done
 
-for i in $(find ./ -print | grep -e 377559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf)
+for i in $(find ./ -print | grep -e "377559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
     mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
 done
