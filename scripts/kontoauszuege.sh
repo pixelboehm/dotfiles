f9ac9d3af84f7ce9e736abc09ac21a702ced057f
Maximilian Fischer <maximilian.fischer@uni-due.de>
2022-11-22 17:50:27 +0100

    add counter in scripts

--- scripts/kontoauszuege.sh
+++ scripts/kontoauszuege.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 YEAR=2021
+COUNT=0
 
 for f in *\ *.pdf; do mv "$f" "${f// /_}"; done
 
 for i in $(find ./ -print | grep -e "7559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
     mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
+    COUNT=$((COUNT=COUNT+1))
 done
 
 for i in $(find ./ -print | grep -e "377559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
     mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
+    COUNT=$((COUNT=COUNT+1))
 done
 
 for i in $(find ./ -print | grep -e "7559119_${YEAR}_.*.pdf")
 do
     mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/Mitteilungen/
+    COUNT=$((COUNT=COUNT+1))
 done
--- scripts/stundenzettel.sh
+++ scripts/stundenzettel.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 YEAR=2022
+COUNT=0
 
 cd ~/Downloads
 for i in $(ls | grep -e KW.*.$YEAR.pdf)
 do
-    mv -i $i ~/Google\ Drive/__02_WORK/ES-Work/Documents/Timesheets/$YEAR/
+    mv -i $i ~/Google\ Drive/__02_WORK/ES-Work/Documents/Timesheets/$YEAR/ 
+    COUNT=$((COUNT=COUNT+1))
 done
+
+echo "Processed $COUNT files."
