b55e29f6950a676aef6f6a8a0dade45cda22e69c
Maximilian Fischer <boehnsins@gmail.com>
2023-04-11 14:40:39 +0200

    change year and path

--- scripts/kontoauszuege.sh
+++ scripts/kontoauszuege.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-YEAR=2021
+YEAR=2023
 COUNT=0
 
-for f in *\ *.pdf; do mv "$f" "${f// /_}"; done
+echo "Processing Year $YEAR"
+
+# for f in *\ *.pdf; do mv "$f" "${f// /_}"; done
 
 for i in $(find ./ -print | grep -e "7559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
-    mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
+    mv -i $i ~/Nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
     COUNT=$((COUNT=COUNT+1))
 done
 
 for i in $(find ./ -print | grep -e "377559119_${YEAR}_Nr.[0-9][0-9][0-9]_Kontoauszug.*.pdf")
 do
-    mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
+    mv -i $i ~/Nextcloud/max/__MF__/__BANKING__/VRB/$YEAR/
     COUNT=$((COUNT=COUNT+1))
 done
 
 for i in $(find ./ -print | grep -e "7559119_${YEAR}_.*.pdf")
 do
-    mv -i $i ~/Documents/nextcloud/max/__MF__/__BANKING__/VRB/Mitteilungen/
+    mv -i $i ~/Nextcloud/max/__MF__/__BANKING__/VRB/Mitteilungen/
     COUNT=$((COUNT=COUNT+1))
 done
